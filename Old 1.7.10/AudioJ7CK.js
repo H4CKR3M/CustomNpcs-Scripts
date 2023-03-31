00c744cbd55f6edda3952a8eb3fd7b0f2679a0e0
H4CKR3M <121751918+H4CKR3M@users.noreply.github.com>
2023-03-31 15:07:03 -0700

    Backported volume support to 1.7.10

--- Old 1.7.10/AudioJ7CK.js
+++ Old 1.7.10/AudioJ7CK.js
@@ -1,40 +1,30 @@
-/* v0.2 - AudioJ2CK | Loadable from Anywhere | Verified 1.12.2+ (1.12.2, 1.16.5) | Written by Rimscar 
+/* v0.2 - AudioJ7CK | Load Script inside INIT, Call Play/Stop/etc from Anywhere | 1.7.10, CustomNPCs_1.7.10d(29oct17) | Written by Rimscar 
  *
  * A better multi-Track audio player to satisfy your creativity!
  * 
  * TO USE:
-    * WAV files must be placed in .minecraft/customnpcs/assets/customnpcs/sounds/audiojack
+    * WAV files must be placed in .minecraft/customnpcs/assets/customnpcs/sounds
     * When playing a sound, write the name WITHOUT the extension: "sound" not "sound.wav"
-    * Example:
-    * Audio.Play("sound")
-    * Audio.Play("sound", audioGain)
+    * To Play, choose ONLY ONE of the following options:
+    *       1. npc.getTempData("audio").Play("wavFileName"); from anywhere.
+    *       2. Audio.Play("wavFileName"); from Init event area ONLY.
     * 
-    * !!! YOU MUST CALL Logout(e) ON PLAYER LOGOUT !!!
-    */
+    * To adjust volume, add a second parameter to Play/Loop like so:  Audio.Play("sample.wav", audioGain) */
 
 var Audio = (function(){
     return {  
-        Init: function Init(e) {                        Audio.P.world = e.API.getIWorld(0); },
-        Logout: function Logout(e) {                    if (e.player.getName() == e.player.world.getAllPlayers()[0].getName()) Audio.StopAll(); },
 
-        Play: function Play(filename){                  Audio.P.StartClip(filename, false, arguments.length > 1 ? arguments[1] : 0); },
-        Loop: function Loop(filename){                  Audio.P.StartClip(filename, true, arguments.length > 1 ? arguments[1] : 0); },
-        Stop: function Stop(filename){                  Audio.P.StopClip(filename); },
-        StopAll: function StopAll(){                    Audio.P.StopAllClips(); },
-        IsPlaying: function IsPlaying(filename) {       return Audio.P.IsPlaying(filename); },
-        Ping: function Ping() {                         Audio.P.say("You have my attention!"); },
+        Play: function Play(filename){                          Audio.Private.StartClip(filename, false, arguments.length > 1 ? arguments[1] : 0); },
+        Loop: function Loop(filename){                          Audio.Private.StartClip(filename, true, arguments.length > 1 ? arguments[1] : 0); },
+        Stop: function Stop(filename){                          Audio.Private.StopClip(filename); },
+        StopAll: function StopAll(){                            Audio.Private.StopAllClips(); },
+        IsPlaying: function IsPlaying(filename) {               return Audio.Private.IsPlaying(filename); },
+        PlayMCSound: function PlayMCSound(soundName, target){   Audio.Private.PlayMCSound(soundName, target); },
+        Ping: function Ping() {                                 npc.say("Pinged the audio Jack!") },
 
-        P: {
-            world: null,
-            keyPrefix: "J2CK_",
-            keyA: "J2CKA",
-            path: "/customnpcs/assets/customnpcs/sounds/audiojack/",
-            debugThrowErrors: true,
+        Private: {
 
-            GetClip: function GetClip(filename){ return this.world.getTempdata().get(this.keyPrefix + filename); },
-            SetClip: function SetClip(filename, clip){  this.world.getTempdata().put(this.keyPrefix + filename, clip); },
-            GetAllFileNames: function GetAllFileNames(){ return this.world.getTempdata().has(this.keyA)?this.world.getTempdata().get(this.keyA):[];},
-            SetAllFileNames: function SetAllFileNames(fNames){ this.world.getTempdata().put(this.keyA, fNames); },
+            clips: [],
 
             StartClip: function StartClip(filename, loop, gain){
                 var AudioSystem = Java.type('javax.sound.sampled.AudioSystem');
@@ -56,7 +46,7 @@ var Audio = (function(){
                 var IOException = Java.type('java.io.IOException');
                 var ByteArrayInputStream = Java.type('java.io.ByteArrayInputStream');
                 
-                var WAV_PATH = new File(System.getProperty("user.dir") + this.path + filename + ".wav");
+                var WAV_PATH = new File(System.getProperty("user.dir") + "/customnpcs/assets/customnpcs/sounds/" + filename + ".wav");
                 var inputStream = null;
                 
                 var validVolume = false;
@@ -76,9 +66,8 @@ var Audio = (function(){
                         if (gain != 0){
                             var gainControls = clip.getControls();
                             for(var i = 0; i < gainControls.length; i++){
-                                if (gainControls[i].getType().toString() == "Master Gain"){
+                                if (gainControls[i].getType().toString() == "Master Gain")
                                     gainControls[i].setValue(gain);
-                                }
                             }
                         }
                         validVolume = true;
@@ -86,34 +75,26 @@ var Audio = (function(){
                         if (loop) { clip.loop(999); }
 
                         var isClipAlreadyInside = false;
-                        var fNames = this.GetAllFileNames();
-                        for(var i = 0; i < fNames.length; i++){
-                            if (fNames[i] == filename)
+                        for(var i = 0; i < this.clips.length; i++){
+                            if (this.clips[i] == clip)
                                 isClipAlreadyInside = true;
                         }
-                        if (!isClipAlreadyInside){
-                            fNames.push(filename);
-                            this.SetAllFileNames(fNames);
-                        }
+                        if (!isClipAlreadyInside)
+                            this.clips.push(clip);
                     }
                     
                 } catch (ex) {
-                    if (this.debugThrowErrors){
-                        if (!foundFile) {
-                            this.say("Unable to locate file §f" + WAV_PATH);
-                            throw ("\n\n[J2CK]: Unable to locate file " + WAV_PATH + "\n\n");
-                        }
-                        if (!validVolume){
-                            this.say("Invalid volume given: §b§l" + gain + "\n§7Try something closer to 0. Like -3.0 to 3.0");
-                            throw("\n\n[J2CK]: Invalid volume given: " + gain + ". Try something closer to 0. Like -3.0 to 3.0\n\n");
-                        }
-                        ex.printStackTrace();
-                    }
+                    if (!foundFile) throw ("\n\n[J7CK]: Unable to locate file " + WAV_PATH + "\n\n");
+                    if (!validVolume) throw("\n\n[J7CK]: Invalid volume given: " + gain + ". Try something closer to 0. Like -3.0 to 3.0\n\n");
+                    ex.printStackTrace();
                 } finally {
                     if (inputStream != null)
                         inputStream.close();
                 }
             },
+
+            GetClip: function GetClip(filename){ return npc.getTempData("J7CK_" + filename); },
+            SetClip: function SetClip(filename, clip){ npc.setTempData("J7CK_" + filename, clip); },
 
             StopClip: function StopClip(filename){
                 var clip = this.GetClip(filename);
@@ -121,11 +102,9 @@ var Audio = (function(){
                     clip.stop();
                     clip.close();
                     this.SetClip(filename, null);
-                    var fNames = this.GetAllFileNames();
-                    for(var i = 0; i < fNames.length; i++){
-                        if (fNames[i] == filename){
-                            fNames.splice(i, 1);
-                            this.SetAllFileNames(fNames);
+                    for(var i = 0; i < this.clips.length; i++){
+                        if (this.clips[i] == clip){
+                            this.clips.splice(i, 1);
                             break;
                         }
                     }
@@ -133,18 +112,15 @@ var Audio = (function(){
             },
 
             StopAllClips: function StopAllClips(){
-                var fNames = this.GetAllFileNames();
-                for(var i = 0; i < fNames.length; i++){
-                    var clip = this.GetClip(fNames[i]);
+                for(var i = 0; i < this.clips.length; i++){
+                    var clip = this.clips[i];
                     if (clip != null){
                         clip.stop();
                         clip.close();
-                        this.SetClip(fNames[i], null);
                     }
-                    fNames.shift();
+                    this.clips.shift();
                     i--;
                 }
-                this.SetAllFileNames(fNames);
             },
 
             IsPlaying: function IsPlaying(filename){
@@ -152,9 +128,17 @@ var Audio = (function(){
                 return clip != null && clip.isOpen() && clip.isRunning();
             },
 
-            say: function say(message){
-                this.world.broadcast("§3[§b§lJ2CK§3]: §7" + message);
+            PlayMCSound: function PlayMCSound(soundName, target){
+                if (target.getType() == 1)
+                    npc.executeCommand("playsound " + soundName + " " + target.getName());
+                else if (target.getType() == 2){
+                    var np = target.getSurroundingEntities(60, 1);
+                    for(var i = 0; i < np.length; i++){
+                        npc.executeCommand("playsound " + soundName + " " + np[i].getName());
+                    }
+                }
             }
         }
     }
 }());
+npc.setTempData("audio", Audio);
